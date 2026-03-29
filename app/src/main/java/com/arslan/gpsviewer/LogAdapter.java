0182f2beda5f14687417e09a45334fad07909c2f
arslanahmedkmlg-byte <arslanahmed.kmlg@gmail.com>
2026-03-29 16:38:05 +0500

    Fix formatting and remove unused view bindings (#1)

--- app/src/main/java/com/arslan/gpsviewer/LogAdapter.java
+++ app/src/main/java/com/arslan/gpsviewer/LogAdapter.java
@@ -1,3 +1,5 @@
+package com.arslan.gpsviewer;
+
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -73,13 +75,6 @@ public class LogAdapter extends RecyclerView.Adapter<LogAdapter.ViewHolder> {
             super(view);
             tvId = view.findViewById(R.id.tvId);
             tvDevice = view.findViewById(R.id.tvDevice);
-            tvTime = view.findViewById(R.id.tvTime);
-            tvLatLng = view.findViewById(R.id.tvLatLng);
-            tvAccuracy = view.findViewById(R.id.tvAccuracy);
-            btnMap = view.findViewById(R.id.btnMap);
-        }
-    }
-}            tvDevice = view.findViewById(R.id.tvDevice);
             tvTime = view.findViewById(R.id.tvTime);
             tvLatLng = view.findViewById(R.id.tvLatLng);
             tvAccuracy = view.findViewById(R.id.tvAccuracy);
